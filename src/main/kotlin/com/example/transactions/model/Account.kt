c1b6b4749271ccb6255189976f0a0a333c0ea9db
David Choloyan <shamsani.david@gmail.com>
2026-05-28 19:52:46 +0200

    feat: account model

---
+++ src/main/kotlin/com/example/transactions/model/Account.kt
@@ -0,0 +1,25 @@
+package com.example.transactions.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.math.BigDecimal
+
+
+@Entity
+@Table(name = "accounts")
+class Account(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = 0,
+
+    @Column(name = "owner", nullable = false)
+    val owner: String,
+
+    @Column(name = "balance", nullable = false, precision = 19, scale = 2)
+    var balance: BigDecimal
+)
